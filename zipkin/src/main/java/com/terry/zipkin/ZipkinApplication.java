954f8a60856a4119a7b5ade3fe016721afc3f377
Terry <2219569347@qq.com>
2018-03-21 10:53:30 +0800

    Zipkin Tracing

---
+++ zipkin/src/main/java/com/terry/zipkin/ZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.terry.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
